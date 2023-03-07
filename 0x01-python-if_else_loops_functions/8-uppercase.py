fb4eae72e125e4838ff89ca6d62f343d6a4b2855
freshCTRL <oyeboabariu25@gmail.com>
2023-03-07 09:55:22 -0800

    otherhalfielse

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+def uppercase(str):
+    strr = list(str)
+    for i in range(len(str)):
+        if (ord(str[i]) >= 95) and (ord(str[i]) <= 122):
+            strr[i] = chr(ord(str[i]) - 32)
+    str = "".join(strr)
+    print("{}".format(str))
