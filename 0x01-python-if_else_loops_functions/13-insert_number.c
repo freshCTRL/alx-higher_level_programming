c2d5215ea2efeea32cda4e5d2efb56fc6e45b6ed
freshCTRL <oyeboabariu25@gmail.com>
2023-03-08 00:05:35 -0800

    Ctask

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * insert_node - Entry point
+ * @head: argument to insert_node
+ * @number: an argument, it stores the data of the inserted node
+ *
+ * Return: NULL(Success)
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+listint_t *ins = malloc(sizeof(listint_t));
+listint_t *mand = *head;
+if (ins == NULL)
+{
+return (NULL);
+}
+ins->n = number;
+ins->next = NULL;
+if (mand != NULL)
+{
+int clear = 0;
+int end = 0;
+while ((mand->next != NULL)  && (clear != 1))
+{
+if (ins->n < mand->next->n)
+{
+if (mand->n > ins->n)
+{
+end = 1;
+}
+clear = 1;
+}
+else
+{
+mand = mand->next;
+}
+}
+if (end != 1)
+{
+ins->next = mand->next;
+mand->next = ins;
+}
+else
+{
+ins->next = mand;
+*head = ins;
+}
+}
+return (NULL);
+}
