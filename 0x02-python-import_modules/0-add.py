6b1425efb5d696772ea4092b6efa9ac10d096ee1
freshCTRL <oyeboabariu25@gmail.com>
2023-03-09 11:13:46 -0800

    importmodule

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,11 +3,10 @@ from add_0 import add
 
 
 def main():
-    pass
-
-
-if __name__ == "__main__":
     a = 1
     b = 2
     print("{} + {} = {}". format(a, b, add(a, b)))
 
+
+if __name__ == "__main__":
+    main()
