60d39dca25ce2808e401025850e29c253b4cf80b
freshCTRL <oyeboabariu25@gmail.com>
2023-03-09 13:55:07 -0800

    importmodule

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1 as add, sub, mul, div
+    from calculator_1 import add, sub, mul, div
+
     a = 10
     b = 5
-    print("{:d} + {:d} = {:d}". format(a, b, a + b))
-    print("{:d} - {:d} = {:d}". format(a, b, a - b))
-    print("{:d} * {:d} = {:d}". format(a, b, a * b))
-    print("{:d} / {:d} = {:d}". format(a, b, int(a / b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
