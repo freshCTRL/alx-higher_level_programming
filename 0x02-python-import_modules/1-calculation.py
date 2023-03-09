1bec3a1c345a9597923cddc66ff5786587f6fc4e
freshCTRL <oyeboabariu25@gmail.com>
2023-03-09 10:58:10 -0800

    importmodules

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+import calculator_1 as calc
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    calc.add(a, b)
+    calc.sub(a, b)
+    calc.mul(a, b)
+    calc.div(a, b)
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def add(a, b):
+    a = a
+    b = b
+    return a + b
