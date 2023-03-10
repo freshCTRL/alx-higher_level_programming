74829db666080a3deb7a2c4f6e7df257410ca708
freshCTRL <oyeboabariu25@gmail.com>
2023-03-09 18:44:27 -0800

    importmodule

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     import hidden_4
     import os
 
-    names = list(os.dir(hidden_4))
+    names = os.dir(hidden_4)
     for name in names:
         if name[0:2] != "__":
             print(name)
