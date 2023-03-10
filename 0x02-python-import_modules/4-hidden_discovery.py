dcdb5b6329fa3a4ca6f9360d646b7cd9056be75e
freshCTRL <oyeboabariu25@gmail.com>
2023-03-09 18:26:05 -0800

    importmodule

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ = "__main__":
     import hidden_4
-    for name_of_functions in dir(hidden_4)
+    names = dir(hidden_4)
+    for name_of_function in names
         if name_of_function[0:2] != "__":
             print(name_of_function)
