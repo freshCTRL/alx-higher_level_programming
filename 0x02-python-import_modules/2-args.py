5098eb59909a51085cd4a8bb0866b296f89cab63
freshCTRL <oyeboabariu25@gmail.com>
2023-03-11 00:00:08 -0800

    importmodule

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     from sys import argv
 
     if len(argv) == 1:
-        print("{:d} argument.".format(len(argv) - 1))
+        print("{} arguments.".format(len(argv) - 1))
     elif len(argv) == 2:
-        print("{:d} argument:".format(len(argv) - 1))
+        print("{} argument:".format(len(argv) - 1))
     else:
-        print("{:d} arguments:".format(len(argv) - 1))
+        print("{} arguments:".format(len(argv) - 1))
 
     for i in range(1, len(argv)):
-        print("{:d}: {}".format(i, argv[i]))
+        print("{}: {}".format(i, argv[i]))
