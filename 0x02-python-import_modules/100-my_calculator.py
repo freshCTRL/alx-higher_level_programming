c66db912f1ad206c6e09f9e5f0ddfa1040cb3859
freshCTRL <oyeboabariu25@gmail.com>
2023-03-11 03:04:04 -0800

    importmodule

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,8 +2,6 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-
-
     if len(sys.argv) != 4:
         print(f"Usage: {sys.argv[0]} <a> <operator> <b>")
         exit(1)
@@ -14,12 +12,16 @@ if __name__ == "__main__":
 
         if operator == '+':
             print("{} {} {} = {}".format(a, operator, b, add(a, b)))
+            exit(0)
         elif operator == '-':
             print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
+            exit(0)
         elif operator == '*':
             print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
+            exit(0)
         elif operator == '/':
             print("{} {} {} = {}".format(a, operator, b, div(a, b)))
+            exit(0)
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
