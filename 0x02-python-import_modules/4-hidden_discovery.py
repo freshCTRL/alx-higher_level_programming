c775ab5759a31b4c60f67569ecc52817d385957b
freshCTRL <oyeboabariu25@gmail.com>
2023-03-11 00:14:54 -0800

    message

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,10 +4,12 @@ if __name__ == "__main__":
 
     import hidden_4
 
-    names = list(dir(hidden_4))
-    names.sort(key=str.lower)
+    names = dir(hidden_4)
+    names = list(names)
+    names = names.sort()
+
     for i in names:
-        if i[:2] != "__":
+        if i[0:2] != "__":
             print(i)
         else:
             continue
