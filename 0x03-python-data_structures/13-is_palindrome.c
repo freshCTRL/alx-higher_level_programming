825f7da964c89ccb2063c9b674cbbe64ce30c3b5
freshCTRL <oyeboabariu25@gmail.com>
2023-03-13 12:25:57 -0700

    datastructure

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+int is_palindrome(listint_t **head) {
+char mylist[50];
+listint_t *mand;
+mand = *head;
+int n = 0;
+while (mand) {
+mylist[n] = mand->n;
+mand = mand->next;
+n++;
+}
+n--;
+int stop = 0;
+int k = n - 1;
+int i = 0;
+while ((i <= k/2) && (stop != 1)) {
+if (mylist[i] != mylist[n]) {
+stop = 1;
+return 0;
+} else {
+if (i == k/2) {
+return 1;
+}
+}
+i++;
+n--;
+}
+return 0;
+}
