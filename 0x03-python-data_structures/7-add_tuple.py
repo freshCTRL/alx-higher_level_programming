f65cb8687b930d55fc3b73a7371e77fa1851d0f9
freshCTRL <oyeboabariu25@gmail.com>
2023-03-13 12:00:13 -0700

    datastructure

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{}".format(matrix[i][j]), end=" ")
-        print()
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = list(tuple_a)
+    tuple_b = list(tuple_b)
+    if len(tuple_a) >= len(tuple_b):
+        for i in range(len(tuple_a)):
+            if len(tuple_b) == 1:
+                tuple_b.insert(1, 0)
+            elif len(tuple_b) == 0:
+                tuple_b.insert(1, 0)
+                tuple_b.insert(1, 0)
+            tuple_a[i] += tuple_b[i]
+        return tuple(tuple_a)
+    else:
+        for i in range(len(tuple_b)):
+            if len(tuple_a) == 1:
+                tuple_a.insert(1, 0)
+            elif len(tuple_b) == 0:
+                tuple_a.insert(1, 0)
+                tuple_a.insert(1, 0)
+            tuple_b[i] += tuple_a[i]
+        return tuple(tuple_a)
