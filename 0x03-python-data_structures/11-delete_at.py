827a8a40517107e4b42a8581a3b051c4535fed2c
freshCTRL <oyeboabariu25@gmail.com>
2023-03-13 12:11:19 -0700

    daatastructure

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    newlist = []
-    for item in my_list:
-        if item % 2 == 0:
-            newlist.append(True)
-        else:
-            newlist.append(False)
-    return newlist
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx > len(my_list):
+        return my_list
+    else:
+        del(my_list[idx])
+        return my_list
