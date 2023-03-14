40c9baa71c87646c2c82855d285c003b8917740d
freshCTRL <oyeboabariu25@gmail.com>
2023-03-14 09:03:18 -0700

    datastructure

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) == 0:
-        return my_list
-    else:
-        for i in reversed(my_list):
+    if len(my_list) != 0:
+        for i in my_list[slice(None, None, -1)]:
             print("{:d}".format(i))
