4f0e6ad51e1f3997e51ec08b3487dd5e19770924
freshCTRL <oyeboabariu25@gmail.com>
2023-03-15 07:41:54 -0700

    datastructure

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,22 +11,25 @@ int is_palindrome(listint_t **head)
 int i = 0, j;
 listint_t *front, *rear, *man;
 
+int s, k, l;
 int count = 0;
 man = *head;
 while (man)
 {
 man = man->next;
-count += 1;
+count++;
 }
-
-while (i != count / 2)
+s = count / 2;
+while (i != s)
 {
 front = rear = *head;
 for (j = 0; j < i; j++)
 {
 front = front->next;
 }
-for (j = 0; j < count - (i + 1); j++)
+l = i + 1;
+k = count - l;
+for (j = 0; j < k; j++)
 {
 rear = rear->next;
 }
