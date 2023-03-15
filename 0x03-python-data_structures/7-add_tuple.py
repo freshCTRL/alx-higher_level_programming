f805412f408c5ba24cb12be0e762467029a3c086
freshCTRL <oyeboabariu25@gmail.com>
2023-03-14 17:30:43 -0700

    datastructure

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,28 +1,28 @@
-#!usr/bin/python3
-def add(a=(), b=()):
-    b = list(b)
-    if len(b) == 1:
-        b.append(0)
-        b = tuple(b)
-    elif len(b) == 0:
-        b.append(0)
-        b.append(0)
-        b = tuple(b)
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_b = list(tuple_b)
+    if len(tuple_b) == 1:
+        tuple_b.append(0)
+        tuple_b = tuple(tuple_b)
+    elif len(tuple_b) == 0:
+        tuple_b.append(0)
+        tuple_b.append(0)
+        tuple_b = tuple(tuple_b)
     else:
-        b = tuple(b)
+        tuple_b = tuple(tuple_b)
 
-    a = list(a)
-    if len(a) == 1:
-        a.append(0)
-        a = tuple(a)
-    elif len(a) == 0:
-        a.append(0)
-        a.append(0)
-        a = tuple(a)
+    tuple_a = list(tuple_a)
+    if len(tuple_a) == 1:
+        tuple_a.append(0)
+        tuple_a = tuple(tuple_a)
+    elif len(tuple_a) == 0:
+        tuple_a.append(0)
+        tuple_a.append(0)
+        tuple_a = tuple(tuple_a)
     else:
-        a = tuple(a)
+        tuple_a = tuple(tuple_a)
 
     c = []
     for i in range(2):
-        c.append(a[i] + b[i])
+        c.append(tuple_a[i] + tuple_b[i])
     return tuple(c)
