fa66445497b87d59b87437fe22256bd79b9d3475
freshCTRL <oyeboabariu25@gmail.com>
2023-03-16 02:47:12 -0700

    datastructure

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is not None:
+        max_key = max(a_dictionary)
+        return a_dictionary[max_key]
+    else:
+        return None
