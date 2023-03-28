088a281b3c0bd7ad4d063200414caf529705500e
freshCTRL <oyeboabariu25@gmail.com>
2023-03-28 16:41:12 -0700

    classes and objects

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -31,4 +31,8 @@ class Square:
     @size.setter
     def size(self, value):
         """set the size property of a square"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
