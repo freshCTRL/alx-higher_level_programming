d8fa850de70d91a639f3b068b18c7a745de4d269
freshCTRL <oyeboabariu25@gmail.com>
2023-03-29 13:27:07 -0700

    classes and objects

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -59,7 +59,7 @@ class Square:
                 print()
             for i in range(self.__size):
                 for x in range(self.__position[0]):
-                    print(' ', end='')
+                    print('_', end='')
                 for j in range(self.__size):
                     print('#', end='')
                 print()
