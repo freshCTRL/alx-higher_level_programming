9da38394ddb3439acd3170e8b9c573d56f38b221
freshCTRL <oyeboabariu25@gmail.com>
2023-03-28 21:43:29 -0700

    exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     new_list = []
-    for l in range(x):
+    for index in range(x):
         try:
-            new_list.append(my_list[l])
+            new_list.append(my_list[index])
         except IndexError:
             continue
     for i in new_list:
