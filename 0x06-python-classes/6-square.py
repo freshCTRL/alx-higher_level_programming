777432fc102293a7061d88394a2544a8b550fafd
freshCTRL <oyeboabariu25@gmail.com>
2023-03-31 19:42:03 -0700

    classes and objects

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -55,10 +55,6 @@ class Square:
             for y in range(self.__position[1]):
                 print()
             for i in range(self.__size):
-                for x in range(self.__position[0]):
-                    print(' ', end='')
-                for j in range(self.__size):
-                    print('#', end='')
-                print()
+                print("{}{}".format(" " * self.__position[0], "#" * self.__size))
         else:
             print()
