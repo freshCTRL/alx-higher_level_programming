cd20f070836983bde5d19ea600762e8c400c1695
freshCTRL <oyeboabariu25@gmail.com>
2023-04-07 07:04:22 -0700

    morecls

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,35 +1,5 @@
 #!/usr/bin/python3
 from sys import argv
-"""
-This line import sys to get the command
-line argument
-"""
-
-"""
-This program takes an integer input from
-the user and solves the N queens problem
-"""
-
-
-"""
-Checks if the length of argument passed
-is not more than 2
-
->>> ./101-nqueens.py
-Usage: nqueens N
-"""
-
-"""Checks if the argument passed is an integer
-
->>> ./101-nqueens.py h
-N must be a number
-"""
-
-"""Checks if the value of N is less than 4
-
->>> ./101-nqueens.py 2
-N must be at least 4
-"""
 
 if len(argv) != 2:
     print("Usage: nqueens N")
@@ -46,19 +16,31 @@ elif int(N) < 4:
 
 elif N.isnumeric() and int(N) >= 4:
     N = int(N)
-    j = []
-    for w in range(N - 2):
-        s = []
-        k = w + 1
-        for i in range(N):
-            if k > N:
-                k -= N+1
-            s.append([i, k])
-            h = w + 2
-            k += h
-        j.append(s)
-    for d in j:
-        print(d)
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+    if N % 2 == 0:
+        j = []
+        for w in range(N - 2):
+            s = []
+            k = w + 1
+            for i in range(N):
+                if k > N:
+                    k -= N+1
+                s.append([i, k])
+                h = w + 2
+                k += h
+            j.append(s)
+        for d in j:
+            print(d)
+    else:
+        j = []
+        for w in range(N - 2):
+            s = []
+            k = w + 2
+            for i in range(N):
+                if k > N:
+                    k -= N+1
+                s.append([i, k])
+                h = w + 2
+                k += h
+            j.append(s)
+        for d in j:
+            print(d)
