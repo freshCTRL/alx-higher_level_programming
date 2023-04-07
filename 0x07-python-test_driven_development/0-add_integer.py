aff99f795ef61c7f61742134ba091017ed368ef8
freshCTRL <oyeboabariu25@gmail.com>
2023-04-07 13:05:37 -0700

    TDD

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """This module contains a function that add two integers(a and b).
 if a float is passed as an argument it cast it into an int.
-if a is not an integer it raises a TypeError.
-if b is not and integer it raises a TypeError.
+if a or b is not an integer it raises a TypeError.
 """
 
 
@@ -19,4 +18,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) != int:
         raise TypeError("b must be an integer")
-    return a + b
+    return "{:d}".format(a+b)
