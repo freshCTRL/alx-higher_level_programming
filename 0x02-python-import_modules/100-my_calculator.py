26606f8d8fed4e4ee23c15b41dabb445d15f0dcf
freshCTRL <oyeboabariu25@gmail.com>
2023-04-08 12:02:03 -0700

    importmodules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,16 +13,12 @@ if __name__ == "__main__":
 
         if operator == '+':
             print("{} {} {} = {}".format(a, operator, b, add(a, b)))
-            exit(0)
         elif operator == '-':
             print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
-            exit(0)
         elif operator == '*':
             print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
-            exit(0)
         elif operator == '/':
             print("{} {} {} = {}".format(a, operator, b, div(a, b)))
-            exit(0)
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
