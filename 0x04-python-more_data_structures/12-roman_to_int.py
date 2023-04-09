256a33b0363f7dd5e9a9bdee01d7a482b3b2691a
freshCTRL <oyeboabariu25@gmail.com>
2023-04-09 03:29:46 -0700

    moredatastructure

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,8 @@ def roman_to_int(roman_string):
     if type(roman_string) != str or roman_string is None:
         return 0
 
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                  'C': 100, 'D': 500, 'M': 1000}
     roman_string = list(roman_string)
     r = []
     for i in roman_string:
