224ea9f28bd066e2daaa3090057311c19b1b330d
freshCTRL <oyeboabariu25@gmail.com>
2023-04-09 13:20:33 -0700

    datastructure

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,55 +2,51 @@
 /**
  * is_palindrome - Entry point.
  * @head: argument to is_palindrome
- *
  * Return: (0) Success
  */
+void reverse(listint_t **head)
+{
+listint_t *temp = NULL, *tp1;
+while (*head)
+{
+tp1 = (*head)->next;
+(*head)->next = temp;
+temp = *head;
+*head = tp1;
+}
+*head = temp;
+}
 int is_palindrome(listint_t **head)
 {
-int i, j, k, count, stop;
-listint_t *front, *rear, *man;
-
-if (*head == NULL)
+int a, k, i, count = 0, stop = 0;
+listint_t *sp = NULL, *tp = *head, *jp = *head;
+while (tp)
 {
-return (1);
-}
-else
-{
-count = 0;
-man = *head;
-while (man)
-{
-man = man->next;
+tp = tp->next;
 count++;
 }
-i = 0;
-k = count / 2;
-stop = 0;
-while ((i < k)&&(stop != 1))
-{
-j = 0;
-front = *head;
-while (j < i)
-{
-front = front->next;
-j++;
-}
-j = 0;
-rear = *head;
-while (j < count - i - 1)
-{
-rear = rear->next;
-j++;
-}
-if (front->n != rear->n)
-stop = 1;
-else
-i++;
-
-}
-if (stop != 1)
+if ((count == 1) || (count == 0))
 return (1);
-
+k = count / 2;
+tp = *head;
+for (a = 0; (a < k); a++)
+tp = tp->next;
+if ((count % 2) == 0)
+sp = tp;
+else
+sp = tp->next;
+reverse(&sp);
+for (i = 0; ((i < k)&&(stop != 1)); i++)
+{
+if (jp->n == sp->n)
+{
+jp = jp->next;
+sp = sp->next;
 }
+else
+stop = 1;
+}
+if (stop == 1)
 return (0);
+return (1);
 }
