bd0f7134363991a27031d4b09184064636750b37
freshCTRL <oyeboabariu25@gmail.com>
2023-04-09 02:08:20 -0700

    TDD

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,10 +9,13 @@ def add_integer(a, b=98):
     """a function that adds two numbers.
     Raises: TypeError("if argument passed is not an integer")
     """
-
     if type(a) == float:
+        if a + 1 == a:
+            raise OverflowError("Float overflow")
         a = int(a)
     if type(b) == float:
+        if b + 1 == b:
+            raise OverflowError("Float overflow")
         b = int(b)
 
     if type(a) != int:
