307fd9d8cadc899320eddf9e23a9d842981ca765
freshCTRL <oyeboabariu25@gmail.com>
2023-04-15 11:26:59 -0700

    inheritance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -31,18 +31,8 @@ class Rectangle(BaseGeometry):
     """
     a Rectangle class: a subclass to BaseGeometry class
     """
-    def integer_validator(self, name, value):
-        """validates the name
-        and value passed
-        """
-        if type(value) != int:
-            raise TypeError(str(name) + " " + "must be an integer")
-        if value <= 0:
-            raise ValueError(str(name) + " " + "must be greater than 0")
-
     def __init__(self, width, height):
-        super().__init__()
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
