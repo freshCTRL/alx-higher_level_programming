84d455eeee9db91ad7336024ac87c39bf3da1b86
freshCTRL <oyeboabariu25@gmail.com>
2023-04-15 11:56:12 -0700

    inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -43,7 +43,8 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
-        return "[Rectangle]" + " " + str(self.__width) + "/" + str(self.__height)
+        return "[Rectangle]" + " " + str(self.__width) + "/"\
+            + str(self.__height)
 
 
 class Square(Rectangle):
