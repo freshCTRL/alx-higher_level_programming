d9258a5a676b0b7cc734e5547f858b5af039ea56
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-04-19 05:19:26 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,22 +5,7 @@ contains the
 Base class
 """
 
-
-class Base:
-    """
-    a base class
-    """
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        """
-        Initialises the base class
-        """
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+from base import Base
 
 
 class Rectangle(Base):
