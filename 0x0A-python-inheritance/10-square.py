b61f8244dca822df0e1abcb73dd2677661f6b069
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-04-19 09:20:31 +0100

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,46 +5,7 @@ containing a method that determines
 the value of area of the geometry
 """
 
-
-class BaseGeometry:
-    """a BaseGeometry
-    class
-    """
-    def area(self):
-        """
-        checks if the argument needed to determine the area is passed
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates the name
-        and value passed
-        """
-        if type(value) != int:
-            raise TypeError(str(name) + " " + "must be an integer")
-        if value <= 0:
-            raise ValueError(str(name) + " " + "must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """
-    a Rectangle class: a subclass to BaseGeometry class
-    """
-    def __init__(self, width, height):
-        super().integer_validator("width", width)
-        self.__width = width
-        super().integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """
-        checks if the argument needed to determine the area is passed
-        """
-        return self.__width * self.__height
-
-    def __str__(self):
-        return "[Rectangle]" + " " + str(self.__width) +\
-            "/" + str(self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
