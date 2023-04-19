1bf797e2e82134bc3e79d377343f5e1627ed6230
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-04-19 08:56:02 +0100

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,27 +5,7 @@ containing a method that determines
 the value of area of the geometry
 """
 
-
-class BaseGeometry:
-    """a BaseGeometry
-    class
-    """
-
-    def area(self):
-        """
-        checks if the argument needed to determine the area is passed
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates the name
-        and value passed
-        """
-        if type(value) != int:
-            raise TypeError(str(name) + " " + "must be an integer")
-        if value <= 0:
-            raise ValueError(str(name) + " " + "must be greater than 0")
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """
