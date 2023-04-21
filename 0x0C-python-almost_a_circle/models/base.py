ebee6826a01fb4d7b80be3ecf210354d3f0fbb39
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-04-21 18:03:07 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,20 +28,25 @@ class Base:
         representation of list_dictionaries:
         """
         import json
-        if len(list_dictionaries) == 0 or list_dictionaries is None:
+        if list_dictionaries is None:
             return json.dumps([])
         else:
-            if type(list_dictionaries) == list:
-                return json.dumps(list_dictionaries)
+            return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
         """a function that writes the JSON string representation
         of list_objs to a file:
         """
+        import json
         a_string = cls.to_json_string(list_objs)
-        with open(__class__.__name__.json, mode="w", encoding="utf-8") as file:
-            file.write(a_string)
+        b_string = json.loads(a_string)
+        if not b_string:
+            with open(__class__.__name__.json, mode="w", encoding="utf-8") as file:
+                file.write(a_string)
+        else:
+            with open(__class__.__name__.json, mode="w", encoding="utf-8") as file:
+                file.write(a_string)
 
     @staticmethod
     def from_json_string(json_string):
@@ -52,3 +57,19 @@ class Base:
             return []
         import json
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """a function  that returns an instance with
+        all attributes already set:
+        """
+        dum = cls(width=1, height=1, x=0, y=0, id=None)
+        return dum.update(**dictionary)
+
+    @classmethod
+    def load_from_file(cls):
+        if not __class__.__name__.is_file():
+            return []
+        else:
+            new_dict = cls.from_json_string(__class__.__name__.json)
+            return cls.create(**new_dict)
