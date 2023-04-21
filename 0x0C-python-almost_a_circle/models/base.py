917c3a47342e2dcd150d69d1c71d3d84b732f3e9
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-04-21 17:00:32 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,3 +21,30 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """a function that that returns the JSON string
+        representation of list_dictionaries:
+        """
+        import json
+        if len(list_dictionaries) == 0:
+            return []
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """a function that writes the JSON string representation
+        of list_objs to a file:
+        """
+        a_string = cls.to_json_string(list_objs)
+        with open(__class__.__name__.json, mode="w", encoding="utf-8") as file:
+            file.write(a_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """a function that returns a list of JSON
+        string representation json_string
+        """
+        import json
+        return json.loads(json_string)
