a38a535b56e48a9faddbfbbb4a64c77dd4d22509
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-04-21 09:32:25 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -147,22 +147,23 @@ class Rectangle(Base):
             if type(args[0]) != dict:
                 if len(args) == 1:
                     self.id = args[0]
-                if len(args) == 2:
+                elif len(args) == 2:
                     self.id = args[0]
                     self.__width = args[1]
-                if len(args) == 3:
+                elif len(args) == 3:
                     self.id = args[0]
                     self.__width = args[1]
                     self.__height = args[2]
-                if len(args) == 4:
+                elif len(args) == 4:
                     self.id = args[0]
                     self.__width = args[1]
                     self.__height = args[2]
                     self.__x = args[3]
-                if len(args) == 5:
+                elif len(args) == 5:
                     self.id = args[0]
                     self.__width = args[1]
                     self.__height = args[2]
+                    self.__x = args[3]
                     self.__y = args[4]
             else:
                 for key, value in args[0].items():
