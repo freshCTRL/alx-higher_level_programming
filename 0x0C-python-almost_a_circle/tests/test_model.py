299d9ca070b8bdafafd0c43addd394196bcc5121
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-08-09 15:56:40 +0100

    Update test_model.py

--- 0x0C-python-almost_a_circle/tests/test_model.py
+++ 0x0C-python-almost_a_circle/tests/test_model.py
@@ -10,22 +10,6 @@ from models.square import Square
 
 
 class Test_1(unittest.TestCase):
-    def test_Base(self):
-        """
-        THis test definition tests all the possible test cases
-        on class Base
-        """
-        b1 = Base(89)
-        self.assertEqual(b1.id, 89)
-#        b2 = Base()
-#        self.assertEqual(b2.id, 2)
-#       ist_3 = Base(99)
-#        self.assertEqual(ist_3.id, 99)
-#       self.assertIsNotNone(ist_3.to_json_string(None))
-#       self.assertIsNotNone(ist_3.to_json_string([]))
-#       self.assertIsNotNone(ist_3.to_json_string([{'id': 12}]))
-#       self.assertIsInstance(ist_3.to_json_string([{'id': 12}]), str)
-#       self.assertIsNotNone(ist_3.from_json_string(None))
-#       self.assertIsNotNone(ist_3.from_json_string("[]"))
-#       self.assertIsNotNone(ist_3.from_json_string('[{"id": 12}]'))
-#       self.assertIsInstance(ist_3.from_json_string('[{"id": 12}]'), list)
+    def test_ifExists(self):
+        b1 = Base()
+        self.assertIsNotNone(b1.id)
