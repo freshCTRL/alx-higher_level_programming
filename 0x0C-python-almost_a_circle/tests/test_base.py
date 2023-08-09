f7c6890817f4fa575ee4e1d1be5563546a117c18
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-08-09 23:26:08 +0100

    Update test_base.py

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,12 +1,9 @@
-import unittest
 """
 THis module contain possible test cases for the
 class Base, Square, and Rectangle
 """
+import unittest
 from models.base import Base
-"""
-importing base
-"""
 
 
 class Test_1(unittest.TestCase):
@@ -14,16 +11,20 @@ class Test_1(unittest.TestCase):
     this is a test class containing all
     possible test cases
     """
+    def setUp():
+        """
+        initialising a test
+        """
+        b1 = Base()
+        b2 = Base()
+        ist_3 = Base(99)
     def test_Base(self):
         """
         THis test definition tests all the possible test cases
         on class Base
         """
-        b1 = Base()
-        self.assertEqual(b1.id, 1)
-        b2 = Base()
-        self.assertEqual(b2.id, 2)
-        ist_3 = Base(99)
+        self.assertTrue(b1.id == 1)
+        self.assertTrue(b2.id == 2)
         self.assertEqual(ist_3.id, 99)
         self.assertIsNotNone(ist_3.to_json_string(None))
         self.assertIsNotNone(ist_3.to_json_string([]))
@@ -33,3 +34,10 @@ class Test_1(unittest.TestCase):
         self.assertIsNotNone(ist_3.from_json_string("[]"))
         self.assertIsNotNone(ist_3.from_json_string('[{"id": 12}]'))
         self.assertIsInstance(ist_3.from_json_string('[{"id": 12}]'), list)
+    def tearDown():
+        """
+        closing a test
+        """
+        del b1
+        del b2
+        del ist_3
