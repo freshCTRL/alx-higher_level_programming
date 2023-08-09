453b223a5581f47399b90d6f322deda42093dc32
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-08-09 15:58:48 +0100

    Update test_model.py

--- 0x0C-python-almost_a_circle/tests/test_model.py
+++ 0x0C-python-almost_a_circle/tests/test_model.py
@@ -1,12 +1,1 @@
-import unittest
-"""
-THis module contain possible test cases for the
-class Base, Square, and Rectangle
-"""
-from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
-"""Import the Base, Square, and the Rectangle class"""
 
-
-class Test_1(unittest.TestCase):
