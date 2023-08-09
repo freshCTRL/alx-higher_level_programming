608b20851c75b0eb07e46aa853ae1f41354a63ae
freshCTRL <oyeboabariu25@gmail.com>
2023-08-09 08:07:14 -0700

    almost

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,31 @@
+import unittest
+"""
+THis module contain possible test cases for the
+class Base, Square, and Rectangle
+"""
+from models.base import Base
+"""
+importing base
+"""
+
+
+class Test_1(unittest.TestCase):
+    def test_Base(self):
+        """
+        THis test definition tests all the possible test cases
+        on class Base
+        """
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        ist_3 = Base(99)
+        self.assertEqual(ist_3.id, 99)
+        self.assertIsNotNone(ist_3.to_json_string(None))
+        self.assertIsNotNone(ist_3.to_json_string([]))
+        self.assertIsNotNone(ist_3.to_json_string([{'id': 12}]))
+        self.assertIsInstance(ist_3.to_json_string([{'id': 12}]), str)
+        self.assertIsNotNone(ist_3.from_json_string(None))
+        self.assertIsNotNone(ist_3.from_json_string("[]"))
+        self.assertIsNotNone(ist_3.from_json_string('[{"id": 12}]'))
+        self.assertIsInstance(ist_3.from_json_string('[{"id": 12}]'), list)
