d882e3ea653ce4556c639fb7e983bee3cd1d5133
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-08-10 23:04:07 +0100

    Update test_base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -36,7 +36,7 @@ class TestBase(unittest.TestCase):
         b5 = Base()
 
         self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 2)
+        self.assertEqual(b2.id, b1.id+1)
         self.assertEqual(b3.id, 3)
         self.assertEqual(b4.id, 99)
         self.assertEqual(b5.id, 4)
