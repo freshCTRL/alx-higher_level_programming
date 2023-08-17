71babaa7d6de2bf2de1fb215b254ac8e37751415
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-08-18 00:46:09 +0100

    mysql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,1 +1,9 @@
 -- a script that lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT 
+tv_shows.title  FROM tv_shows 
+INNER JOIN tv_show_genres        
+ON tv_shows.id = tv_show_genres.show_id       
+INNER JOIN tv_genres        
+ON tv_genres.id = tv_show_genres.genre_id        
+WHERE tv_shows.title = 'Comedy'
+ORDER BY tv_shows.title;```
