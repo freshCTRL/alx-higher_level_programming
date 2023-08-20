b6ff74f404fe3722d60604b93de693d117dae750
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-08-20 05:42:23 +0100

    Update test_rectangle.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -88,17 +88,8 @@ class Test_2(unittest.TestCase):
         val = {"id": 89, "width": 2, "height": 2, "x": 3, "y": 4}
         sample3 = Rectangle.create(**val)
         self.assertEqual(str(sample3), '[Rectangle] (89) 3/4 - 2/2')
-
-    def test_empty_file(self):
-        """
-            test save and load method
-        """
         Rectangle.save_to_file([])
         self.assertTrue(Rectangle.load_from_file() == [])
-    def test_empty_file(self):
-        """
-            test save and load method
-        """
         val = [Rectangle(1, 2)]
         Rectangle.save_to_file(val)
         self.assertTrue(Rectangle.load_from_file()[0].width == 1)
