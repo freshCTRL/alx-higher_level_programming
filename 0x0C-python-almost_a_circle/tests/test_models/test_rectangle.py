e83deb827beb68896ba35ac267875ef196c5170f
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-08-20 01:56:03 +0100

    Update test_rectangle.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -91,7 +91,6 @@ class Test_2(unittest.TestCase):
         Rectangle.save_to_file(None)
         self.assertEqual(Rectangle.load_from_file(), [])
         Rectangle.save_to_file([])
-        self.assertIsInstance(Rectangle.load_from_file(), str)
         val = [Rectangle(1, 2)]
         Rectangle.save_to_file(val)
         self.assertIsInstance(Rectangle.load_from_file(), list)
