c93bab9f6c1910ec61b6a06da0efb6540e75ffb0
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-08-20 05:58:31 +0100

    Update test_square.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -66,10 +66,10 @@ class Test_3(unittest.TestCase):
         sample2 = Square(1, 2, 3, 4)
         self.assertEqual(sample2.to_dictionary(),
                          {'id': 4, 'size': 1, 'x': 2, 'y': 3})
-        Square.save_to_file([])
-        self.assertTrue(Square.load_from_file() == [])
         val = None
         Square.save_to_file(val)
+        self.assertTrue(Square.load_from_file() == [])
+        Square.save_to_file([])
         self.assertTrue(Square.load_from_file() == [])
         val = Square(1)
         Square.save_to_file([val])
