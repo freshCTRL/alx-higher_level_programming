ccbaf73d7f04b4f9a007824c904bb002094f0233
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-08-20 04:33:29 +0100

    Update test_square.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -64,7 +64,7 @@ class Test_3(unittest.TestCase):
         Square.save_to_file(val)
         self.assertTrue(Square.load_from_file() == [])
         Square.save_to_file([])
-        self.assertTrue(Square.load_from_file() == [])
+        self.assertEqual(Square.load_from_file(), [])
         val = Square(1)
         Square.save_to_file([val])
         self.assertTrue(Square.load_from_file()[0].size == 1)
