7b1f3354de950915c1fe18fc512784d9cc88fbcd
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-08-20 08:51:00 +0100

    Update test_square.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -47,10 +47,6 @@ class Test_3(unittest.TestCase):
         self.assertRaises(ValueError, Square, 1, -2)
         self.assertRaises(ValueError, Square, 1, 2, -3)
         self.assertRaises(ValueError, Square, 0)
-        Square.save_to_file(None)
-        self.assertEqual(Square.load_from_file(), [])
-        Square.save_to_file([])
-        self.assertEqual(Square.load_from_file(), [])
         self.assertEqual(str(sample1), '[Square] (28) 2/3 - 1')
         sample1.update()
         self.assertEqual(str(sample1), '[Square] (28) 2/3 - 1')
@@ -70,9 +66,13 @@ class Test_3(unittest.TestCase):
         sample2 = Square(1, 2, 3, 4)
         self.assertEqual(sample2.to_dictionary(),
                          {'id': 4, 'size': 1, 'x': 2, 'y': 3})
+        Square.save_to_file(None)
+        self.assertEqual(Square.load_from_file(), [])
         val = Square(1)
         Square.save_to_file([val])
         self.assertEqual(Square.load_from_file()[0].size, 1)
+        Square.save_to_file([])
+        self.assertEqual(Square.load_from_file(), [])
 
     def test_square_display(self):
         """
